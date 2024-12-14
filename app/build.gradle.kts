db9fe3145e37337d7a9c54c5a7c3f5fbb10b4148
qamarelsafadi <qamar.elsafadi98@gmail.com>
2024-12-14 20:17:43 +0200

    (fix) : update compile sdk to match the AGP version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.gdgevents.gdgeventsapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.gdgevents.gdgeventsapp"
