9d851b6bfa277b34c03ab099e8c896e1499b1fac
nabilBouzineDev <nabilsocialnetwork@gmail.com>
2024-12-21 21:23:40 +0100

    added the splash screen

--- app/src/main/java/com/gdgevents/gdgeventsapp/MainActivity.kt
+++ app/src/main/java/com/gdgevents/gdgeventsapp/MainActivity.kt
@@ -4,19 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gdgevents.gdgeventsapp.ui.theme.GDGEventsAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         enableEdgeToEdge()
+
         setContent {
             GDGEventsAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -36,12 +42,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GDGEventsAppTheme {
-        Greeting("Android")
-    }
 }
