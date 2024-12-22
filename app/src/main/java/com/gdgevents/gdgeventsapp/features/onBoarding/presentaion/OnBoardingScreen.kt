c7e07b3fcb4961b18e7b77fe08fe06c93f1f920e
MCD <debchouneharoun@gmail.com>
2024-12-22 11:05:51 +0100

    adding images for onboarding

--- app/src/main/java/com/gdgevents/gdgeventsapp/features/onBoarding/presentaion/OnBoardingScreen.kt
+++ app/src/main/java/com/gdgevents/gdgeventsapp/features/onBoarding/presentaion/OnBoardingScreen.kt
@@ -1,11 +1,15 @@
 package com.gdgevents.gdgeventsapp.features.onBoarding.presentaion
 
+import android.widget.Button
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun OnBoardingScreen(
+    getStarted: () -> Unit,
+    nextButton: () -> Unit,
+    skipButton: () -> Unit,
     viewModel: OnBoardingViewModel = hiltViewModel(),
 ) {
-    // add ui
+
 }
--- app/src/main/java/com/gdgevents/gdgeventsapp/features/onBoarding/presentaion/OnBoardingViewModel.kt
+++ app/src/main/java/com/gdgevents/gdgeventsapp/features/onBoarding/presentaion/OnBoardingViewModel.kt
@@ -6,5 +6,5 @@ import javax.inject.Inject
 
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor() : ViewModel() {
-    // add implementation
+
 }
