a8d39fc952b5e4d96deee4019a39033eb90a61a7
MCD <debchouneharoun@gmail.com>
2025-01-18 21:07:30 +0100

    set up On Boarding with navigation

--- app/src/main/java/com/gdgevents/gdgeventsapp/features/onBoarding/presentaion/OnBoardingScreen.kt
+++ app/src/main/java/com/gdgevents/gdgeventsapp/features/onBoarding/presentaion/OnBoardingScreen.kt
@@ -25,16 +25,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.gdgevents.gdgeventsapp.GdgAppState
 import com.gdgevents.gdgeventsapp.R
 import com.gdgevents.gdgeventsapp.features.onBoarding.presentaion.components.GetStartedScreen
 import com.gdgevents.gdgeventsapp.features.onBoarding.presentaion.components.OnboardingTextContainer
 import com.gdgevents.gdgeventsapp.features.onBoarding.presentaion.model.onBoardList
 import com.gdgevents.gdgeventsapp.features.onBoarding.util.WindowType
 import com.gdgevents.gdgeventsapp.features.onBoarding.util.rememberWindowSize
-import com.gdgevents.gdgeventsapp.ui.theme.GDGEventsAppTheme
 import ir.kaaveh.sdpcompose.sdp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -42,7 +40,8 @@ import kotlinx.coroutines.launch
 @Composable
 fun OnboardingScreen(
     modifier: Modifier = Modifier,
-    onItemClicked: () -> Unit,
+    appState: GdgAppState,
+    onItemClicked: () -> Unit = {appState.goToMapScreen()}
 ) {
     Log.d("OnboardingScreen", "OnboardingScreen started")
     val pagerState = rememberPagerState(pageCount = { onBoardList.size + 1 }, initialPage = 0)
@@ -223,17 +222,6 @@ private fun MediumScreen(
 
 }
 
-
-@Preview(showBackground = true, device = Devices.PIXEL_TABLET)
-@Composable
-private fun OnBoardModelPrev1() {
-    GDGEventsAppTheme {
-        OnboardingScreen(
-            onItemClicked = {}
-        )
-    }
-
-}
 
 
 
